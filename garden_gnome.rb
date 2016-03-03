8499ddfe974af339550d07152f746b8d39c4888b
Tara Campbell <tac1998@gmail.com>
2016-03-03 13:49:22 -0600

    final OOD Garden Gnome

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,55 @@
-# Code your instances here
+class GardenGnome
+
+	def initialize(hat_color="red", personality="evil")
+		@hat_color = hat_color
+		@personality = personality
+
+		@name
+		@age
+		@gluten_allergy
+	end
+
+	def personality
+		return @personality
+	end
+
+	def hat_color
+		return @hat_color
+	end
+
+	def name=(name)
+		@name = name
+	end
+	def name
+		return @name
+	end
+
+	def age=(age)
+		@age = age
+	end
+	def age
+		return @age
+	end
+
+	def gluten_allergy=(gluten_allergy)
+		@gluten_allergy = true
+	end
+	def gluten_allergy
+		return @gluten_allergy
+	end
+
+	def gnaw
+		return "Gnawing on a tree!!!"
+	end
+
+	def shout
+		return "GNARLY!!!"
+	end
+
+	def introduce_self
+		return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+	end
+
+
+end
+
